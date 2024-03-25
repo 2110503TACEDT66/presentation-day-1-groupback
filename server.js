c04eeb54f8d5fc5636c5de40b66ff9a7c1611784
Parietal <6633031721@student.chula.ac.th>
2024-03-25 15:18:10 +0700

    changes to support deployment 1

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.use('/api/v1/bookings',bookings);
 app.use('/api/v1/users',users);
 
 const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT,console.log('Server running in ',process.env.NODE_ENV,' mode on port ',PORT));
+const server = app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} on ${process.env.HOST}:${PORT}`));
 
 //Handle unhandled promise rejections
 process.on('unhandledRejection',(err,promise)=>{
